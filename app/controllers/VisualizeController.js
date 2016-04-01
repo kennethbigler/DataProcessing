474a13754ca6386c182fd03be5802de6596e400e
kennethbigler <kennethbigler@gmail.com>
2016-03-31 17:16:26 -0700

    updates

--- app/controllers/VisualizeController.js
+++ app/controllers/VisualizeController.js
@@ -2,30 +2,12 @@
 
 app.controller('VisualizeController', ['$scope', 'VisualizeService', function ($scope, $VS) {
     "use strict";
-
-    // declare variables
-    var i, asum, bsum;
-    
     // graph vars
     $scope.data = $VS.data;
     $scope.expected = $VS.expected;
+    $scope.max = $VS.max;
     $scope.width = 600;
     $scope.height = 350;
     $scope.yAxis = 'Number';
     $scope.xAxis = 'Time';
-    $scope.max = 0;
-    
-    // find max to fit results into the graph
-    for (i = 0; i < $scope.data.length; i += 1) {
-        if ($scope.data[i].a > $scope.max) {
-            $scope.max = $scope.data[i].a;
-        }
-        if ($scope.data[i].b > $scope.max) {
-            $scope.max = $scope.data[i].b;
-        }
-        if ($scope.expected > $scope.max) {
-            $scope.max = $scope.expected;
-        }
-    }
-    //console.log($scope.data);
 }]);
--- app/services/VisualizeService.js
+++ app/services/VisualizeService.js
@@ -1,10 +1,12 @@
+/*global app, console*/
 app.factory('VisualizeService', [function () {
     "use strict";
     var factory = {},
         data = {},
         i = 0,
         aSum = 0,
-        bSum = 0;
+        bSum = 0,
+        max = 0;
     
     data = window.data.reported;
     // find the sums
@@ -21,6 +23,20 @@ app.factory('VisualizeService', [function () {
     factory.data = data;
     // expected value
     factory.expected = 1 / data.length;
+    
+    // find max to fit results into the graph
+    for (i = 0; i < data.length; i += 1) {
+        if (data[i].a > max) {
+            max = data[i].a;
+        }
+        if (data[i].b > max) {
+            max = data[i].b;
+        }
+        if (factory.expected > max) {
+            max = factory.expected;
+        }
+    }
+    factory.max = max;
 
     return factory;
 }]);
