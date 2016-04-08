0604c02669824aec3fd97e717164c2ebd87d4299
kennethbigler <kennethbigler@gmail.com>
2016-04-07 19:20:10 -0700

    added data swapping

--- app/controllers/VisualizeController.js
+++ app/controllers/VisualizeController.js
@@ -3,11 +3,26 @@
 app.controller('VisualizeController', ['$scope', 'VisualizeService', function ($scope, $VS) {
     "use strict";
     // graph vars
-    $scope.data = $VS.data;
-    $scope.expected = $VS.expected;
-    $scope.max = $VS.max;
     $scope.width = 600;
     $scope.height = 350;
     $scope.yAxis = 'Number';
     $scope.xAxis = 'Time';
+    
+    function init() {
+        $scope.data = $VS.data;
+        $scope.max = $VS.max;
+        $scope.aExpected = $VS.aExpected;
+        $scope.bExpected = $VS.bExpected;
+    }
+    init();
+    
+    $scope.swapData = function () {
+        if ($scope.data === $VS.data) {
+            $scope.max = $VS.nMax;
+            $scope.data = $VS.nData;
+            $scope.aExpected = $scope.bExpected = $VS.expected;
+        } else {
+            init();
+        }
+    };
 }]);
--- app/services/VisualizeService.js
+++ app/services/VisualizeService.js
@@ -2,27 +2,16 @@
 app.factory('VisualizeService', [function () {
     "use strict";
     var factory = {},
-        data = {},
+        data = [],
+        nData = [],
         i = 0,
         aSum = 0,
         bSum = 0,
         max = 0;
     
-    data = window.data.reported;
-    // find the sums
-    for (i = 0; i < data.length; i += 1) {
-        aSum += data[i].a;
-        bSum += data[i].b;
-    }
-    // divide value by sum for %
-    for (i = 0; i < data.length; i += 1) {
-        data[i].a /= aSum;
-        data[i].b /= bSum;
-    }
-    // assign to return value
+    // get data from the window
+    data = window.data;
     factory.data = data;
-    // expected value
-    factory.expected = 1 / data.length;
     
     // find max to fit results into the graph
     for (i = 0; i < data.length; i += 1) {
@@ -37,6 +26,43 @@ app.factory('VisualizeService', [function () {
         }
     }
     factory.max = max;
+    
+    // find the sums
+    for (i = 0; i < data.length; i += 1) {
+        aSum += data[i].a;
+        bSum += data[i].b;
+    }
+    
+    // expected value
+    factory.aExpected = aSum / data.length;
+    factory.bExpected = bSum / data.length;
+    
+    // Normalize Data
+    // divide value by sum for %
+    for (i = 0; i < data.length; i += 1) {
+        nData.push({a: (data[i].a / aSum), b: (data[i].b / bSum)});
+    }
+    // assign to return value
+    factory.nData = nData;
+    
+    // find max to fit results into the graph
+    max = 0;
+    for (i = 0; i < nData.length; i += 1) {
+        if (nData[i].a > max) {
+            max = nData[i].a;
+        }
+        if (nData[i].b > max) {
+            max = nData[i].b;
+        }
+        if (factory.expected > max) {
+            max = factory.expected;
+        }
+    }
+    // assign to return value
+    factory.nMax = max;
+    
+    // expected value
+    factory.expected = 1 / data.length;
 
     return factory;
 }]);
